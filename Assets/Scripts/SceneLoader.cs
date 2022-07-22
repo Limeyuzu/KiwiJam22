8984aba7c5b39b32f07729cf16fe14d51ab8e05d
Yu Li <miiyusli@gmail.com>
2022-07-23 11:55:03 +1200

    add generic scene scripts

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        //FindObjectOfType<GlobalSingleton>().ResetState();
+    }
+
+    public static IEnumerator LoadGameOverSceneCoroutine(float delaySeconds)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        SceneManager.LoadScene("GameOverScene");
+    }
+
+    public static void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public static IEnumerator LoadNextScene(float delay = 0)
+    {
+        yield return new WaitForSeconds(delay);
+        LoadNextScene();
+    }
+
+    public static void QuitGame()
+    {
+        Application.Quit();
+    }
+}
