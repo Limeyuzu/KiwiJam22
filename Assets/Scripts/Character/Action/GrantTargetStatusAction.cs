e33a59c7f9f3af2a58f2470e83a69de4054796a2
Yu Li <miiyusli@gmail.com>
2022-07-24 12:46:36 +1200

    add grant target status action

---
+++ Assets/Scripts/Character/Action/GrantTargetStatusAction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace AdventureTogether
+{
+    public class GrantTargetStatusAction : BaseCharacterAction, ICharacterAction
+    {
+        [SerializeField] ValuedCharacterStatus GrantStatus;
+
+        public override IEnumerator Act(INamed thisNamed, Party party, Character target, TextMeshProUGUI textOutput)
+        {
+            yield return textOutput.AddBattleText($"{thisNamed.Name} gives {GrantStatus.Name} to {target.Name}: ", ActionTextDelaySeconds);
+            yield return target.ReceiveStatus(GrantStatus, textOutput);
+        }
+    }
+}
